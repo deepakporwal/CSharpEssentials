3c8751c28ca91033a545d0caf3ed8636ddb219f8
deepakporwal <er.deepakporwal@gmail.com>
2022-04-28 11:12:18 +0530

    Add project files.

---
+++ CSharpEssentials/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "deepak";
+Console.WriteLine("Hello, World! God is great");
+Console.WriteLine($"My name is {name}"); // $ to pass parameters to string
+
